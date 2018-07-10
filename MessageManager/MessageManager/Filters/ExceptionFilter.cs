28226e1d2ee292c346632030d703fa5f7baa3e90
Unknown <dedalexij@mail.ru>
2018-07-10 17:07:31 +0300

    changed project structure

---
+++ MessageManager/MessageManager/Filters/ExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using MessageManagerLib.Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+
+
+namespace MessageManager.Filters
+{
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (!context.ExceptionHandled)
+            {
+                var msg = context.Exception.Message;
+                Log.Error(msg);
+            }
+
+            switch (context.Exception)
+            {
+                case UserAlreadyExistsException exception:
+                    context.Result = new BadRequestResult();
+                    break;
+            }
+        }
+    }
+}
